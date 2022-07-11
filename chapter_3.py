3421da99b8d8a336911914a76e2f3f1217b92b22
Andrew <atklenner@gmail.com>
2022-07-11 15:04:26 -0700

    numbers numbers numbers, it's all I think about

---
+++ chapter_3.py
@@ -0,0 +1,6 @@
+seconds_per_hour = 60 * 60
+print(seconds_per_hour)
+print(seconds_per_hour * 24)
+seconds_per_day = seconds_per_hour * 24
+print(seconds_per_day / seconds_per_hour)
+print(seconds_per_day // seconds_per_hour)
