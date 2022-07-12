18e86a07ea3f5b07f3999072b0d2dccec040e1b9
Andrew <atklenner@gmail.com>
2022-07-12 11:01:22 -0700

    finish chapter 4 to do

---
+++ chapter_4.py
@@ -0,0 +1,23 @@
+secret = 7
+guess = 5
+
+if guess < secret:
+  print("too low")
+elif guess > secret:
+  print("too high")
+else:
+  print("just right")
+
+small = False
+green = False
+
+if small:
+  if green:
+    print("pea")
+  else:
+    print("cherry")
+else:
+  if green:
+    print("watermelon")
+  else:
+    print("pumpkin")
