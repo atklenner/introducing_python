99a543473823804093665243eb73864d59558e6d
Andrew Klenner <atklenner@gmail.com>
2022-07-31 23:39:45 -0700

    the author doesn't understand how classes work

--- test.py
+++ test.py
@@ -1,10 +1,22 @@
-position = 0
-while position < 5:
-  position += 1
-  if position % 2:
-    print("even")
-    continue
-else:
-  print("odd")
+from distutils import file_util
 
-  
+
+class Fruit:
+  color = "red"
+
+blueberry = Fruit()
+print(Fruit.color)
+print(blueberry.color)
+
+blueberry.color = "blue"
+print(blueberry.color)
+print(Fruit.color)
+
+Fruit.color = "orange"
+print(Fruit.color)
+print(blueberry.color)
+del blueberry.color
+print(blueberry.color)
+
+new_fruit = Fruit()
+print(new_fruit.color)
